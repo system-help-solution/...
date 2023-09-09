43f96e9b6f7f4b6c004f0d4578f62655ca20d30a
jefferson system help <144559525+system-help-solution@users.noreply.github.com>
2023-09-09 18:00:06 -0300

    Update configurar.sh

--- configurar.sh
+++ configurar.sh
@@ -11,7 +11,7 @@ echo -e "\e[32m▀███████████ ▄██   ███ ▀█
 echo -e "\e[32m         ███ ███   ███          ███     ███       ███    █▄  ███   ███   ██▄\e[0m"
 echo -e "\e[32m   ▄█    ███ ███   ███    ▄█    ███     ███       ███    ███ ███   ███   ██▄\e[0m"
 echo -e "\e[32m ▄████████▀   ▀█████▀   ▄████████▀     ▄████▀     ██████████  ▀█   ███   █▀\e[0m"
-echo -e "\e[32m           By SystemHelp MOD V. 0.0.1\e[0m"
+echo -e "\e[32m           By SystemHelp MOD V. 0.1.3\e[0m"
 echo -e "\e[32m                          APOIA AQUI ESTA O PIX CNPJ: 48.590.314/0001-18 \e[0m"
 echo -e "\e[32m                          Telegram https://t.me/+FGzk0EiNths1N2Nh \e[0m"
 echo -e "\e[32m                          YOUTUBE https://www.youtube.com/@SYSTEMHELP\e[0m"
