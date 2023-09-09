0da94421040480fc95e6399d68263a720a6c38ff
jefferson system help <144559525+system-help-solution@users.noreply.github.com>
2023-09-09 18:45:27 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -8,16 +8,60 @@ display_banner() {
   else
     echo "Banner não encontrado. Verifique se o arquivo banner.sh está na mesma pasta."
   fi
-  sleep 1  # Tempo de exibição do banner (1 segundos)
+  sleep 3  # Tempo de exibição do banner (3 segundos)
 }
 
 # Exibe o banner informativo
 display_banner
 
-# Dar permissões de execução ao script configurar.sh
-chmod +x configurar.sh
+# Função para exibir o menu
+display_menu() {
+  echo "===================================="
+  echo "        Instalador Automático       "
+  echo "===================================="
+  echo
+  echo "Escolha uma opção para instalação:"
+  echo "1. Instalar Evolution API"
+  echo "2. Instalar Chatwoot 2.18.0 Edition"
+  echo "3. Instalar Chatwoot 3.0 Edition"
+  echo "4. Sair"
+  echo
+}
 
-# Executa o script configurar.sh
-./configurar.sh
+# Exibe o menu
+display_menu
 
+# Ler a opção escolhida pelo usuário
+read -p "Digite o número da opção desejada e pressione Enter: " option
+
+# Executa a ação correspondente à opção escolhida
+case $option in
+  1)
+    # Chama o configurar.sh antes de instalar o Evolution API
+    chmod +x configurar.sh
+    ./configurar.sh
+    # Chama o evo.sh após o configurar.sh
+    chmod +x evo.sh
+    ./evo.sh
+    ;;
+  2)
+    # Dar permissão de execução ao chatwoot_2_18.sh
+    chmod +x chatwoot_2_18.sh
+    ./chatwoot_2_18.sh
+    ;;
+  3)
+    # Dar permissão de execução ao chatwoot_3_0.sh
+    chmod +x chatwoot_3_0.sh
+    ./chatwoot_3_0.sh
+    ;;
+  4)
+    echo "Saindo do instalador."
+    exit 0
+    ;;
+  *)
+    echo "Opção inválida. Por favor, escolha uma opção válida."
+    ;;
+esac
+
+# Fim do script
 exit 0
