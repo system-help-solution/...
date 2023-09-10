0447b912ea6a2b122762be3b3d78eea4576b69f4
jefferson system help <144559525+system-help-solution@users.noreply.github.com>
2023-09-10 18:53:04 -0300

    Update ngx.sh

--- ngx.sh
+++ ngx.sh
@@ -1,77 +1,79 @@
 #!/bin/bash
 
-# Função para exibir o banner do arquivo banner.sh
-display_banner() {
-  clear
-  if [ -f banner.sh ]; then
-    source banner.sh
-  else
-    echo "Banner não encontrado. Verifique se o arquivo banner.sh está na mesma pasta."
-  fi
-  sleep 3  # Tempo de exibição do banner (3 segundos)
-}
+# Atualize o sistema
+sudo apt update
+sudo apt upgrade -y
 
-# Exibe o banner informativo
-display_banner
+# Instale as dependências
+sudo apt install -y ca-certificates curl gnupg2 apt-transport-https lsb-release
 
-curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && docker swarm join-token manager && apt update -y && apt install docker-compose -y && docker network create npm_public && docker volume create --name=npm_data && docker volume create --name=npm_letsencrypt
+# Adicione o repositório oficial do Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Atualize o repositório e instale o Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-cd #
+# Instale o Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-echo "Criando a pasta 'ngx'..."
-mkdir -p ngx
+# Crie os diretórios para o Nginx Proxy Manager
+sudo mkdir -p /etc/nginx-proxy/{data,letsencrypt}
 
-# Navegar até a pasta 'ngx'
-cd ngx
-
-# Criar o arquivo 'docker-compose.yml' com as informações inseridas
-echo "Criando o arquivo 'docker-compose.yml'..."
-cat <<EOL >docker-compose.yml
-version: '3.3'
+# Crie o arquivo de composição do Docker para o Nginx Proxy Manager
+cat > /etc/nginx-proxy/docker-compose.yml <<EOL
+version: "3"
 services:
   app:
     image: 'jc21/nginx-proxy-manager:latest'
     restart: unless-stopped
-    volumes:
-      - npm_data:/data
-      - npm_letsencrypt:/etc/letsencrypt
     ports:
-      # These ports are in format <host-port>:<container-port>
-      - '81:80' # Public HTTP Port
-      - '443:443' # Public HTTPS Port
-      - '84:84' # Admin Web Port
-    deploy:
-      replicas: 1
-      placement:
-        constraints:
-          - node.role == manager
-      resources:
-        limits:
-          cpus: "0.1"
-          memory: 256M
-    networks:
-      - npm_public
-volumes:
-  npm_data:
-    external: true
-  npm_letsencrypt:
-    external: true
+      - '80:80'
+      - '443:443'
+      - '81:81'
+    environment:
+      DB_MYSQL_HOST: "db"
+      DB_MYSQL_PORT: 3306
+      DB_MYSQL_USER: "nginxproxy"
+      DB_MYSQL_PASSWORD: "S3NHA_NGINX_PR0XY"
+      DB_MYSQL_NAME: "nginxproxy"
+    volumes:
+      - /etc/nginx-proxy/data:/data
+      - /etc/nginx-proxy/letsencrypt:/etc/letsencrypt
+    depends_on:
+      - db
 
-networks:
-  npm_public:
-    external: true
+  db:
+    image: 'jc21/mariadb-aria:latest'
+    restart: unless-stopped
+    environment:
+      MYSQL_ROOT_PASSWORD: 'S3NHA_D3_R00T'
+      MYSQL_DATABASE: 'nginxproxy'
+      MYSQL_USER: 'nginxproxy'
+      MYSQL_PASSWORD: 'S3NHA_NGINX_PR0XY'
+    volumes:
+      - /etc/nginx-proxy/data/mysql:/var/lib/mysql
 EOL
 
-# Executar docker-compose
-echo "Executando 'docker-compose up -d'..."
-docker-compose up -d
+# Navegue até o diretório do Nginx Proxy Manager e inicie os contêineres
+cd /etc/nginx-proxy
+sudo docker-compose up -d
 
-if [ $? -eq 0 ]; then
-  echo "Nginx Proxy Manager foi iniciado com sucesso!"
-else
-  echo "Erro ao iniciar o Nginx Proxy Manager. Verifique as dependências e tente novamente."
-fi
+# Aguarde até que os contêineres sejam iniciados
+sleep 10
 
-# Voltar para o instalador.sh
-exec ./instalador.sh
+# Exiba os contêineres em execução
+sudo docker ps -a
+
+echo "A instalação do Nginx Proxy Manager foi concluída. Acesse http://SEU_IP_SERVER/ para configurá-lo."
+echo "Use o seguinte login inicial:"
+echo "Email: admin@example.com"
+echo "Senha: changeme"
+
+cd #
+cd ...
+
+./instalador.sh
