bf843059a2f40ab416693582b5d13a306f3b5c0e
jefferson system help <144559525+system-help-solution@users.noreply.github.com>
2023-09-10 09:45:18 -0300

    Create banner2.sh

---
+++ banner2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Banner de créditos
+echo "
+███████╗██╗  ██╗███████╗    ██╗███████╗██╗  ██╗███████╗
+██╔════╝██║  ██║██╔════╝    ██║██╔════╝██║  ██║██╔════╝
+███████╗███████║███████╗    ██║███████╗███████║███████╗
+╚════██║██╔══██║╚════██║    ██║╚════██║██╔══██║╚════██║
+███████║██║  ██║███████║    ██║███████║██║  ██║███████║
+╚══════╝╚═╝  ╚═╝╚══════╝    ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
+
+Créditos para André Almeida e ao canal 'Fabricando Sua Ideia Tutoriais'.
+Para mais informações, visite https://www.youtube.com/@fabricandosuaideiatutoriais
+Cláudio Vinicius da comunidade Evolution-Api (cobaia master kkk)
+"
