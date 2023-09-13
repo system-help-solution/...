357916c935ab6cf3958bc8a72c16ef0cd931f74b
jefferson system help <144559525+system-help-solution@users.noreply.github.com>
2023-09-13 20:17:41 -0300

    Update typeboot+nginx.sh

--- typeboot+nginx.sh
+++ typeboot+nginx.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Função para exibir o banner do arquivo banner.sh
+display_banner() {
+  clear
+  if [ -f banner.sh ]; then
+    source banner.sh
+  else
+    echo "Banner não encontrado. Verifique se o arquivo banner.sh está na mesma pasta."
+  fi
+  sleep 3  # Tempo de exibição do banner (3 segundos)
+}
+
+# Exibe o banner informativo
+display_banner
+
 #######################################################
 
 
